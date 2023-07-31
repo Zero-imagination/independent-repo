f454e6781b9fc6b307a8e282ad6a95122a25a585
Konstantin Pshenichnikov <kpshenichnikov@phoenixit.ru>
2023-07-31 03:04:13 +0300

    add column annotation for CommonAbstractAttributes and BaseAbstractSystemAttributes

--- src/main/java/com/disagreed/independentrepo/model/CommonAbstractAttributes.java
+++ src/main/java/com/disagreed/independentrepo/model/CommonAbstractAttributes.java
@@ -1,33 +1,44 @@
 package com.disagreed.independentrepo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+
 
 /**
  * Класс для общих параметров.
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@MappedSuperclass
 public abstract class CommonAbstractAttributes extends BaseAbstractSystemAttributes{
 
     /**
      * Площадь.
      */
     @EqualsAndHashCode.Exclude
+    @Column(name = "area")
     private Long area;
 
     /**
      * Население.
      */
     @EqualsAndHashCode.Exclude
+    @Column(name = "population")
     private Long population;
 
     /**
      * Плотность населения чел./км^2.
      */
     @EqualsAndHashCode.Exclude
+    @Column(name = "density")
     private Long density;
 
     /**
      * Координаты местоположения - ширина и долгота.
      */
     @EqualsAndHashCode.Exclude
+    @Column(name = "coordinates")
     private String coordinates;
 }
