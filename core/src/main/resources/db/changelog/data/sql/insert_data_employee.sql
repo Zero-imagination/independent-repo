abed3402c82c531d2b96de8a710fb4236e92b930
Zero-imagination <56300366+zero-imagination@users.noreply.github.com>
2024-02-14 15:34:03 +0300

    add more insert_data for migration

--- core/src/main/resources/db/changelog/data/sql/insert_data_employee.sql
+++ core/src/main/resources/db/changelog/data/sql/insert_data_employee.sql
@@ -1,4 +1,7 @@
 insert into employee (person_id, employee_type_id, qualification_id, restaurant_id)
 values (3, 1, 5, 1),
        (4, 2, 1, 1),
-       (5, 3, 1, 1);
+       (5, 3, 1, 1),
+       (13, 1, 5, 2),
+       (14, 2, 1, 2),
+       (15, 3, 1, 2)
